ade591210496f14c60318460713602fe1ce0197a
FeruValk <extrememundo@gmail.com>
2024-04-18 17:57:03 -0300

    Criação da Janela com o jogo da delha centralizado

--- src/main/Jogo.java
+++ src/main/Jogo.java
@@ -1,5 +1,75 @@
 package main;
 
-public class Jogo {
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class Jogo extends JPanel{
+	
+	private JFrame window;
+	private JPanel panel;
+
+	private BufferedImage imgGame;
+	private boolean gameOver;
+	private char[][] board;
+	private int turn;
+	
+	public Jogo() {
+		this.gameOver = false;
+		this.board = new char[2][2];
+		this.turn = 0;
+		loadImages();
+		
+	}
+	
+	public void GameLoop() {
+
+	}
+	
+	public void CreatePanel() {
+		
+	}
+	
+	public void loadImages() {
+		try {
+			imgGame = ImageIO.read(new File("src/resources/JogoDaVelha.png"));
+			repaint();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}	
+	
+	
+	public void Draw() {
+		
+		
+	}
+	
+	@Override
+	public void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		if(imgGame != null) {
+			int xImageGame = (getWidth() - imgGame.getWidth())/2;
+			int yImageGame = (getHeight() - imgGame.getHeight())/2;
+			g.drawImage(imgGame, xImageGame, yImageGame, this);
+		}else {
+			System.out.println("Error: Image not found");
+		}
+		
+	}
+	
+	public JPanel getPanel() {
+		return panel;
+	}
+
+	public void setPanel(JPanel panel) {
+		this.panel = panel;
+	}
+	
 }
--- src/main/Window.java
+++ src/main/Window.java
@@ -1,8 +1,35 @@
+package main;
 
+import javax.swing.JFrame;
 
 public class Window {
 
+	private JFrame mainWindow;
+
+	private static int Width = 900;
+	private static int Height = 800;
+	
+	public Window() {
+		this.createWindow();
+		Jogo jogo = new Jogo();
+		mainWindow.add(jogo);
+		mainWindow.setVisible(true);
+	}
+
     public void createWindow(){
-        Stage jogoDaVelha = new Stage();
+        JFrame window = new JFrame("Jogo da Velha");
+        window.setSize(Width, Height);
+        window.setResizable(false);
+        window.setVisible(true);
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);   
+        this.setMainWindow(window);
     }
+    
+    public JFrame getMainWindow() {
+		return mainWindow;
+	}
+
+	public void setMainWindow(JFrame mainWindow) {
+		this.mainWindow = mainWindow;
+	}
 }
